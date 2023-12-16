12d5fa3621556a57771348b86e6a37cb85f3c703
ckhui <ckhanghui@gmail.com>
2023-12-16 14:48:22 +0800

    add docker run script

---
+++ docker_run.sh
@@ -0,0 +1,8 @@
+docker run -it -d -p 3000:3000 \
+   -e OPENAI_API_KEY=sk-xxxx \
+   -e CODE=your-password \
+   -v /Users/ckh/Documents/THU/baidu_llm/ChatGPT-Next-Web/:/app2/ yidadaa/chatgpt-next-web \
+   /bin/sh
+
+
+
