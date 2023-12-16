b0ff18e09c183064348bc07cca0a5d54fa8ad047
ckhui <ckhanghui@gmail.com>
2023-12-16 15:19:47 +0800

    Resize window and rename

--- app/constant.ts
+++ app/constant.ts
@@ -46,8 +46,8 @@ export enum StoreKey {
   Sync = "sync",
 }
 
-export const DEFAULT_SIDEBAR_WIDTH = 300;
-export const MAX_SIDEBAR_WIDTH = 500;
+export const DEFAULT_SIDEBAR_WIDTH = 800;
+export const MAX_SIDEBAR_WIDTH = 800;
 export const MIN_SIDEBAR_WIDTH = 230;
 export const NARROW_SIDEBAR_WIDTH = 100;
 
